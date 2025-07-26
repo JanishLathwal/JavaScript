139db391c32ea37da3236afc014c9520bafed522
JanishLathwal <janishlathwal3@gmail.com>
2025-07-26 11:36:54 +0000

    Definition

---
+++ 06_DOM/one.js
@@ -0,0 +1,6 @@
+// DOM :- Document Object Model!
+// A Tree like structure of the HTML web page in which we call each element as node! eg. text node etc. 
+
+// basically it is a structural diagram of the HTML code in tree str.
+
+// When a web page is loaded, the browser turns the HTML into a tree-like structure called the DOM. It represents all the elements on the page (like headings, paragraphs, buttons, etc.) in a way that programming languages like JavaScript can interact with.
