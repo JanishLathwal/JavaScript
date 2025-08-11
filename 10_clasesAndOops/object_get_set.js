2d2b4f0dbecea098c2f39af738726bb1ab1b00fb
JanishLathwal <janishlathwal3@gmail.com>
2025-08-11 12:26:27 +0000

    get set in object

---
+++ 10_clasesAndOops/object_get_set.js
@@ -0,0 +1,18 @@
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+
+// this is a factory function!
+const tea = Object.create(User)
+console.log(tea.email);
